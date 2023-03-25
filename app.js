dd81528ce632bbc9acbb3bd8b4f2453f5538dc30
Bransholm <kenneth@bransholm.dk>
2023-03-26 00:46:21 +0100

    Updates
    Updates

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 "use strict";
+window.addEventListener("load", initApp);
+
+const newToDo = document.querySelector("#input-new-to-do");
+const list = document.querySelector("#to-do-list");
+
+function initApp() { 
+    document.querySelector("btn-add-item").addEventListener("click; addToDo");
+
+};
+
+function addToDo() {};
+
+function removeToDo() { };
+
+const myHTML = `
+<li>
+<span>${newToDo.value}</span>
+<button>Add</button>
+</li>
+`;
+
+list.insertAdjacentHTML("beforeend", myHTML);
